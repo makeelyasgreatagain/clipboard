b5b1e0d7b2e161423a54da817beeb18343bc2580
Elyas <elysignup@gmail.com>
2025-08-30 19:21:52 +0330

    Update clipboard.py
    Added timestamp to clipboard.

--- clipboard.py
+++ clipboard.py
@@ -8,10 +8,11 @@ last_text = ""
 while True:
     try:
         text = pyperclip.paste()
+        log_time = str(round(time.time()))
         if text != last_text and text.strip() != "":
             with open("clipboard.txt", "a", encoding="utf-8") as f:
-                f.write(text + "\n")
-            print(f"Saved: {text[:40]}...")
+                f.write(log_time + ": " + text + "\n")
+            print(f"Saved: {log_time}: {text[:40]}...")
             last_text = text
         time.sleep(1)
     except KeyboardInterrupt:
